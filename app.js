e580f273f8c5118c80e98f3b6b77b7694acc3987
Veron Maximo <maximoveron49@gmail.com>
2025-06-23 10:43:13 -0300

    integracion de la constante PORT y primer prueba de conexion al servidor

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+import express from 'express';
 import sequelize from './config/database.js';
 import character from './models/chaaracters.model.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('funciona');
+});
 
 try {
     await sequelize.authenticate();
@@ -9,3 +20,7 @@ try {
 } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
 }
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
