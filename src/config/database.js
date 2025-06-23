d2b8d1270414291b46d4cdb05370b13cba0f9fb1
Veron Maximo <maximoveron49@gmail.com>
2025-06-23 10:00:07 -0300

    configuracion inicial de json, estructuracion de database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const {sequelize}=require('sequelize');
+import dotenv from 'dotenv';
+dotenv.config();
+const sequelize = new sequelize(
+process.env.DB_NAME,
+process.env.DB_USER,
+process.env.DB_PASSWORD,
+{
+    host:process.env.DB_HOST,
+    dialect: 'mysql',
+}
+);
+
+export default sequelize;
