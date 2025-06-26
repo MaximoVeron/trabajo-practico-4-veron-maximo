e89ee4edb34bcc39fefb8b00fe3ab97b7c550e4c
Maximo Veron <maximoveron49@gmail.com>
2025-06-26 14:46:34 -0300

    finalizacion del proyecto

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,19 @@ const sequelize = new Sequelize(
 process.env.DB_NAME,
 process.env.DB_USER,
 process.env.DB_PASSWORD,
-{
-    host:process.env.DB_HOST,
-    dialect: 'mysql',
-}
+    {
+        host:process.env.DB_HOST,
+        dialect: 'mysql',
+    }
 );
 
 export default sequelize;
+export const initDB = async () => {
+    try{
+        await sequelize.authenticate();
+        console.log("conexion MySQL establecida");
+        await sequelize.sync();
+    } catch (error) {
+        console.error("error al conectar a la base de datos", error);
+    }
+};
